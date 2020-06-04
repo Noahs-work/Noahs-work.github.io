2b656a994598430984d4d6761364db458b0dcb2b
Noah's work <61124600+Noahs-work@users.noreply.github.com>
2020-06-04 02:31:19 -0400

    Add files via upload

---
+++ github/pp.js
@@ -0,0 +1,14 @@
+function s_h(){
+
+  var click = document.getElementById("Cont");
+  if(click.style.display ==="none"){
+
+    click.style.display = "block";
+
+  }else{
+
+    click.style.display = "none";
+
+  }
+
+}
